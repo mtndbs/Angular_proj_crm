f3c0123a00dab3f95ffd8d343b29c6e33b7d3e7e
mtndbs <mtndbs@gmail.com>
2023-04-24 14:29:40 +0300

    some bugs

--- src/app/employees/employees-page/employees-page.component.ts
+++ src/app/employees/employees-page/employees-page.component.ts
@@ -66,7 +66,7 @@ export class EmployeesPageComponent implements OnInit {
     console.log('refresh');
     setTimeout(() => {
       this.searchEmp = this.employees;
-    }, 300);
+    }, 400);
   }
 
   searchFunc() {
